01e1152609613ed0e2bdccdf783e679666f8fd84
Darma <inemani841@gmail.com>
2026-07-24 21:44:41 +0700

    auto create sqlite database and migrate on vercel

--- api/index.php
+++ api/index.php
@@ -4,6 +4,18 @@
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['APP_VIEW_COMPARED_PATH'] = '/tmp';
+
+// Otomatis buat database SQLite dan jalankan migrate jika belum ada
+$dbPath = '/tmp/database.sqlite';
+if (!file_exists($dbPath)) {
+    touch($dbPath);
+    
+    // Boot Laravel secara minimal untuk menjalankan migrate
+    require __DIR__ . '/../vendor/autoload.php';
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
+    $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+    $kernel->call('migrate', ['--force' => true]);
+}
 
 require __DIR__ . '/../public/index.php';
