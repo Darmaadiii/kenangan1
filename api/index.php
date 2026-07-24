04a3b96cebad9cf708bb8fadc2fa588a7addbab8
Darma <inemani841@gmail.com>
2026-07-24 21:14:03 +0700

    fix storage tmp vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,9 @@
 <?php
+
+// Mengatur direktori penyimpanan agar mengarah ke /tmp Vercel yang writable
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
 require __DIR__ . '/../public/index.php';
