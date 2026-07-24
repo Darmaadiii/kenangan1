56a3b26d37a5333d47fb34d64a81c379c5fcf5d1
Darma <inemani841@gmail.com>
2026-07-24 21:55:03 +0700

    fix view compiled path to tmp

--- api/index.php
+++ api/index.php
@@ -4,14 +4,13 @@
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
-$_ENV['APP_VIEW_COMPARED_PATH'] = '/tmp';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp'; // Pastikan kuncinya VIEW_COMPILED_PATH
 
 // Otomatis buat database SQLite dan jalankan migrate jika belum ada
 $dbPath = '/tmp/database.sqlite';
 if (!file_exists($dbPath)) {
     touch($dbPath);
-
-    // Boot Laravel secara minimal untuk menjalankan migrate
+    
     require __DIR__ . '/../vendor/autoload.php';
     $app = require_once __DIR__ . '/../bootstrap/app.php';
     $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
